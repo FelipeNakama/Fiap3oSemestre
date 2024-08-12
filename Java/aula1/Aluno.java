65ee62be14dd496135f6ce4bfdff6bc896d3657e
Felipe <113949309+FelipeNakama@users.noreply.github.com>
2024-08-12 10:59:20 -0300

    Add files via upload

---
+++ Java/aula1/Aluno.java
@@ -0,0 +1,18 @@
+package aula1;
+
+public class Aluno {
+
+	String nome;
+	short idade;
+	String genero;
+	String matricula;
+	
+	void estudar() {
+		System.out.println("Estudar");
+	}
+	
+	void comer() {
+		System.out.println("Comer");
+	}
+	
+}
