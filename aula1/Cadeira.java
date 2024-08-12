fc7b8fc1b4980695b20f3f0dfc486699096cf8ad
Felipe <113949309+FelipeNakama@users.noreply.github.com>
2024-08-12 09:52:59 -0300

    Add files via upload

---
+++ aula1/Cadeira.java
@@ -0,0 +1,14 @@
+package aula1;
+
+public class Cadeira {
+
+	String material;
+	short apoios;
+	short altura;
+	String encosto;
+	
+	void movimentar() {
+		System.out.println("Mover a cadeira ali");
+	}
+	
+}
---
+++ aula1/Mesa.java
@@ -0,0 +1,16 @@
+package aula1;
+
+public class Mesa {
+
+	String material;
+	short tamanho;
+	String cor;
+	
+	void apoiar() {
+		System.out.println("Apoiando um teclado");
+	}
+	
+	void segurar() {
+		System.out.println("Segurando uma tela");
+	}
+}
